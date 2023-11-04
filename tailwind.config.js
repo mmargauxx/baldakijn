ca3f027d429479b8560628f44bee554791b19190
Margaux Laurier <margaux.laurier@gmail.com>
2023-11-04 15:41:32 +0100

    coming soon

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,16 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'cream': '#f8f1e2',
+      'blue': '#000068',
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        'garamond' : ['garamond-premier-pro', 'serif'],
       },
     },
   },
