3377ed70b8a37129d3055a5b06dc47f531aa9cfc
Margaux Laurier <margaux.laurier@gmail.com>
2023-11-04 15:52:23 +0100

    update favicon

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       {/* <h1 className="text-4xl font-garamond font-bold uppercase text-cream tracking-widest">Baldakijn</h1> */}
       <Image src="/logo-baldakijn.png" width={500} height={500} />
-            <h1 className="text-4xl font-garamond font-normal uppercase text-cream tracking-widest">Coming Soon</h1>
+      <h1 className="text-4xl font-garamond font-normal uppercase text-cream tracking-widest">Coming Soon</h1>
 
     </main>
   )
